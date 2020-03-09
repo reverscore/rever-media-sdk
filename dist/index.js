e1a80d12389c720e1a3e2e7325914e806c628fa6
Arturo Meza <jart.mez@gmail.com>
2020-03-09 17:22:06 -0600

    Generate dist version on pre-commit hook

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _init = _interopRequireDefault(require("./init"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = {
+  init: _init.default
+};
