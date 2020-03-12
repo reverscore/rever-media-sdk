30d2901c2f67125a403e0087f47afcbc4ebca4a2
Arturo Meza <jart.mez@gmail.com>
2020-03-12 15:11:34 -0600

    Do not track dist files in git and ensure they are generated in the prepublish phase

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _init = _interopRequireDefault(require("./init"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {
-  init: _init.default
-};
